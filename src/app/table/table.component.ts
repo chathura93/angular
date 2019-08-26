786cfc1926f09f1d8658ca5d2f0b199e5d38b07e
NishadiKamburugamuwa <nishadink@gmail.com>
2019-08-26 14:46:28 +0530

    angular

---
+++ src/app/table/table.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.css']
+})
+export class TableComponent implements OnInit {
+
+
+   ex = "isha";
+   ex2="kr";
+
+   student = {
+     firstName : "THARINDU",
+     lastName : "Rukshan",
+     city: "kurunegala"
+   }
+
+   student1 = {
+    firstName : "Lahiru",
+    lastName : "Rukshan",
+    city: "kurunegala"
+  }
+
+  studentArray :any=[this.student,this.student1]
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
